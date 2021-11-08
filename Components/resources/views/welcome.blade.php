22963a806f56c645e8127a1f07d46cceeed350a0
A-Hashemian <amin@hashemian.tech>
2021-11-08 22:21:48 +0300

    syntax error

--- Components/resources/views/welcome.blade.php
+++ Components/resources/views/welcome.blade.php
@@ -19,6 +19,6 @@
    </home>
       </div>
 
-       <script src="{{asset{'js/app.js'}}}" charset="utf-8"></script>
+       <script src="{{asset('js/app.js')}}" charset="utf-8"></script>
     </body>
 </html>
